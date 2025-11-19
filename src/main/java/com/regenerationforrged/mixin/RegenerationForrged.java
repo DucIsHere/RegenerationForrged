e2ccf2dd9936fcc292e7d29afbfcaff75802866a
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-11-19 12:49:24 +0700

    Update RegenerationForrged.java

--- src/main/java/com/regenerationforrged/mixin/RegenerationForrged.java
+++ src/main/java/com/regenerationforrged/mixin/RegenerationForrged.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(RegenerationForrged.class)
 public class RegenerationForrged {
 	@Inject(at = @At("HEAD"), method = "loadLevel")
 	private void init(CallbackInfo info) {
