19b3a98507d2991604e5263b1f9b2af9b3bbef57
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-11-19 12:51:55 +0700

    Rename class to MixinRegenerationForrged

--- src/main/java/com/regenerationforrged/mixin/MixinRegenerationForrged.java
+++ src/main/java/com/regenerationforrged/mixin/MixinRegenerationForrged.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RegenerationForrged.class)
-public class RegenerationForrged {
+public class MixinRegenerationForrged {
 	@Inject(at = @At("HEAD"), method = "loadLevel")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftServer.loadLevel()V
