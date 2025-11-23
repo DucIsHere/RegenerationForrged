b526dc6af45b53d467fdc98ba776f83cf552754e
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-11-23 21:03:41 +0700

    Create MixinRandomState.java

---
+++ src/main/java/com/regenerationforrged/mixin/MixinRandomState.java
@@ -0,0 +1,16 @@
+package regenerationforrged.mixin;
+
+import net.minecraft.world.level.levelgen.RandomState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RandomState.class)
+public class MixinRandomState {
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void initSeedSync(long seed, CallbackInfoReturnable<Void> cir) {
+        RegenerationForrgedPipeline.getInstance().setSeed(seed);
+    }
+}
