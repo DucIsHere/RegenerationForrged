7cac39d5d7e50e0f98f2322463df2e6db44fda1d
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-11-23 21:04:20 +0700

    Create MixinBiomeGenerationSettings.java

---
+++ src/main/java/com/regenerationforrged/mixin/MixinBiomeGenerationSettings.java
@@ -0,0 +1,16 @@
+package regenerationforrged.mixin;
+
+import net.minecraft.world.level.levelgen.biome.BiomeGenerationSettings;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BiomeGenerationSettings.class)
+public class MixinBiomeGenerationSettings {
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void injectCustomBiomeRules(CallbackInfo ci) {
+        RegenerationForrgedBiome.applyBiomeMask(this);
+    }
+}
