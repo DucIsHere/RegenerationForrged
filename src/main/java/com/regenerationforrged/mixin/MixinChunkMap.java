d9b8cf16fdb646539476ea57cc00b4523d2f22e2
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-11-24 19:00:42 +0700

    Update MixinChunkMap.java

--- src/main/java/com/regenerationforrged/mixin/MixinChunkMap.java
+++ src/main/java/com/regenerationforrged/mixin/MixinChunkMap.java
@@ -1,4 +1,4 @@
-package regenerationforrged.mixin;
+package com.regenerationforrged.mixin;
 
 import net.minecraft.world.level.chunk.ChunkMap;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,5 +11,10 @@ public class MixinChunkMap {
     @Redirect(method = "updateChunks", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/ChunkAccess;setBiome(Ljava/lang/Object;II)V"))
     private void redirectBiomePlacement(Object biome, int x, int z, Object chunk) {
         RegenerationForrgedBiome.placeBiome((ChunkAccess)chunk, x, z);
+    }
+
+    @Inject(method = "updateChunkTracking", at = @At("HEAD"))
+    private void rgf$trackRGFChunks(ServerPlayer player, ChunkPos pos, CallBackInfo ci) {
+        RGFDebugTracker.track(player, pos);
     }
 }
