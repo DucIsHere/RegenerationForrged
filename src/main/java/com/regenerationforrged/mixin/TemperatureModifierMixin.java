17225b3775833295aee8c72577f65ae27edc07ad
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-11-24 19:07:11 +0700

    Update TemperatureModifierMixin.java

--- src/main/java/com/regenerationforrged/mixin/TemperatureModifierMixin.java
+++ src/main/java/com/regenerationforrged/mixin/TemperatureModifierMixin.java
@@ -1,4 +1,4 @@
-package regenerationforrged.mixin;
+package com.regenerationforrged.mixin;
 
 import net.minecraft.world.level.levelgen.temperature.TemperatureModifier;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,5 +11,11 @@ public class TemperatureModifierMixin {
     @Redirect(method = "modifyTemperature", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/temperature/TemperatureSampler;sample(DD)D"))
     private double redirectTemperature(double x, double z) {
         return RegenerationForrgedPipeline.getInstance().temperatureSample(x, z);
+    }
+
+    @Inject(methoof = "modifyTemperature", at = @At("HEAD"), cancellable = true)
+    private void rgf$overrideTemp(BlockPos pos, float base, CallBackInfoReturnable<Float> cir) {
+        float t = RGFThermalHandler.get(pos, base);
+        if (!Float.isNaN(t)) cir.getReturnValue(t);
     }
 }
