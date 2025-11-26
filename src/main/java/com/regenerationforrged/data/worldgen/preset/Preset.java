1fa783de49c5dea3d67a29400d90be880638a3d0
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-11-26 11:41:41 +0700

    Update and rename RGFPreset.java to Preset.java

--- src/main/java/com/regenerationforrged/data/worldgen/preset/Preset.java
+++ src/main/java/com/regenerationforrged/data/worldgen/preset/Preset.java
@@ -1,11 +1,11 @@
 package com.regenerationforrged.data.worldgen.preset;
 
-public class RGFPreset {
+public class Preset {
     private final MiscellaneousSettings misc;
     private final CaveSettings cave;
     private final RGFRiverSettings river;
 
-    public RGFPreset(MiscellaneousSettings misc, CaveSettings cave, RGFRiverSettings river) {
+    public Preset(MiscellaneousSettings misc, CaveSettings cave, RGFRiverSettings river) {
         this.misc = misc; this.cave = cave; this.river = river;
     }
 
