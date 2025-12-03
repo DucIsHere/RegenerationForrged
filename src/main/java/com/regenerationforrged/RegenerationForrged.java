c64aa987f7461783fd27b6f01a0cbdd24eaee91c
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-12-03 19:39:29 +0700

    Update RegenerationForrged.java

--- src/main/java/com/regenerationforrged/RegenerationForrged.java
+++ src/main/java/com/regenerationforrged/RegenerationForrged.java
@@ -2,12 +2,39 @@ package com.regenerationforrged;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.world.level.levelgen.GeneratorType;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.core.Registry;
+import net.minecraft.world.level.biome.Biome;
+
+import net.minecraft.world.level.levelgen.chunk.ChunkGeneratorSettings;
+import net.minecraft.world.level.levelgen.flat.FlatChunkGenerator;
+import net.minecraft.world.level.levelgen.flat.FlatChunkGeneratorConfig;
+import net.minecraft.world.level.levelgen.StructuresConfig;
+
+import java.util.Collections;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RegenerationForrged implements ModInitializer {
 	public static final String MOD_ID = "regenerationforrged";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final GeneratorType VOID = new GeneratorType("void") {
+		protected ChunkGenerator getChunkGenerator(Registry<Biome> biomeRegistry,
+        Registry<ChunkGeneratorSettings> chunkGeneratorSettingsRegistry, long seed) {
+        FlatChunkGeneratorConfig config = new FlatChunkGeneratorConfig(
+        new StructuresConfig(Optional.empty(), Collections.emptyMap()), biomeRegistry);
+        config.updateLayerBlocks();
+        return new FlatChunkGenerator(config);
+
+	};
+	
+
+
+
 
 	@Override
 	public void onInitialize() {
